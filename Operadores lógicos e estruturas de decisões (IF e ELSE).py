907a0888105f22e6a2d2632059c70e58bcdef349
Matheus Campos <103076645+Matheuscamp@users.noreply.github.com>
2025-01-20 14:02:14 -0300

    Create Operadores lógicos e estruturas de decisões (IF e ELSE).py

---
+++ Operadores lógicos e estruturas de decisões (IF e ELSE).py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+if idade >= 18 and altura >= 1.70:
+    print("Pode ser feito o Alistamento Militar")
+else:
+    print("Não pode ser feito o Alistamento Militar!")
+
